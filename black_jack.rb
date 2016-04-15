1a64d58332c4fd5810c2a8d2feca94bc8c86de93
Elijah Rogers <elijahrogers@charter.net>
2016-04-14 22:31:42 -0700

    Mostly functioning correctly

--- black_jack.rb
+++ black_jack.rb
@@ -2,5 +2,5 @@ APP_ROOT = File.dirname(__FILE__)
 $:.unshift( File.join(APP_ROOT, 'lib'))
 require "#{APP_ROOT}/lib/game"
 
-game = Game.new
-game.launch!
+$game = Game.new
+$game.launch!
--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ require 'card'
 #Classes
 class Game
   class Config
-    @@actions = ['new', 'quit']
+    @@actions = ['new', 'quit', 'hit', 'stand', 'new game']
     def self.actions
       return @@actions
     end
@@ -23,17 +23,8 @@ class Game
     until result == :quit
       action = get_action
       result = game_action(action)
-      until @over == true do
-        decision
-      end
-      if @hand == 21 && @dealer_hand != 21
-        win
-      elsif @hand > @dealer_hand && @hand <= 21
-        win
-      else
-        lose
-      end
     end
+
   end
 
   def get_action
@@ -48,49 +39,47 @@ class Game
   def game_action(action)
     case action
     when "new"
-      new
+      @hand < 21 ? new : nil
     when "quit"
-      return :quit
+      quit
+    when "hit"
+      game_action("new")
+    when "stand"
+      @hand < 21 ? game_over : nil
+    when "new game"
+      @hand = 0
+      @card_count = 0
+      @dealer_hand = 0
+      game_action("new")
     end
   end
 
   def decision
-    until @hand >= 21 || @over == true
       output_game_header("You have #{@card_count} cards totaling #{@hand}.",
       "Enter \'hit\' to hit", "Enter \'stand\' to stand")
-      dec = gets.downcase.chomp
-      case dec
-      when "hit"
-        game_action("new")
-      when "stand"
-        @over = true
-      else
-        redo
-      end
-    end
-    @dealer_hand += Card.new.value
-    @over = true
   end
 
   def new
-    case @card_count
-    when 0
-      first_card = Card.new
-      second_card = Card.new
-      @hand = first_card.value
-      @hand += second_card.value
-      @card_count += 2
-      dealer_card = Card.new
-      @dealer_hand = dealer_card.value
-      output_game_header("Your first card is the #{first_card.type} of #{$suits.sample}.",
-      "Your second card is the #{second_card.type} of #{$suits.sample} ",
-      "The dealer has the #{dealer_card.type} of #{$suits.sample} showing.")
-    else
-      next_card = Card.new
-      @hand += next_card.value
-      @card_count += 1
-      output_game_header("Your new card is the #{next_card.type} of #{$suits.sample}.")
-    end
+      case @card_count
+      when 0
+        first_card = Card.new
+        second_card = Card.new
+        dealer_card = Card.new
+        @dealer_hand = dealer_card.value
+        output_game_header("Your first card is the #{first_card.type} of #{$suits.sample}.",
+        "Your second card is the #{second_card.type} of #{$suits.sample} ",
+        "The dealer has the #{dealer_card.type} of #{$suits.sample} showing.")
+        @hand = first_card.value
+        @hand += second_card.value
+        @card_count += 2
+      else
+        next_card = Card.new
+        output_game_header("Your new card is the #{next_card.type} of #{$suits.sample}.")
+        @hand += next_card.value
+        @card_count += 1
+      end
+    @hand == 21 ? game_over : nil
+    @hand > 21 ? game_over : decision
   end
 
   def win
@@ -101,6 +90,41 @@ class Game
   def lose
     output_game_header("You lost. Better luck next time!",
     "You had #{@hand}", "The dealer had #{@dealer_hand}")
+  end
+
+  def quit
+    output_game_header("Are you sure you want to quit (yes/no)?")
+    answer = nil
+    until ["yes", "no"].include?(answer)
+    answer = gets.chomp
+      if answer == "yes"
+        output_game_header("Thanks for playing Blackjack!", "Goodbye!")
+        return :quit
+      elsif answer == "no"
+        @card_count == 0 ? intro : decision
+      else
+        output_game_header("I'm sorry, I don't understand that command.","Enter \'yes\' to quit","Enter \'no\' to continue")
+      end
+    end
+  end
+
+  def game_over
+    puts @dealer_hand
+    @dealer_hand += (1 + rand(9))
+    if @hand == 21 && @dealer_hand != 21
+      win
+    elsif @hand > 21
+      lose
+    elsif @hand > @dealer_hand
+      win
+    else
+      nil
+    end
+    new_game
+  end
+
+  def new_game
+    output_game_header("Would you like to play again?","Enter \'new game\' to restart")
   end
 
   def get_hand
@@ -131,6 +155,3 @@ class Game
   end
 
 end
-=begin
-
-=end
